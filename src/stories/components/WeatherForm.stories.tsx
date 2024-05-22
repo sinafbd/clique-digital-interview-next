e58548b1b62111102850e0d9acd1d35932317a68
sina fbd <sina.farbod@gmail.com>
2024-05-22 13:14:33 +0200

    feat: add WeatherForm story

---
+++ src/stories/components/WeatherForm.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from "@storybook/react";
+import WeatherForm from "../../app/components/WeatherForm";
+
+export default {
+  title: "Components/WeatherForm",
+  component: WeatherForm,
+} as Meta;
+
+type Story = StoryObj<typeof WeatherForm>;
+
+export const Primary: Story = {
+  args: {
+    onSearch: (city: string) => alert(`Searching for ${city}`),
+  },
+};
