daeb2c20e766d58007f7fe1d2c0a8d41abaa7d91
sina fbd <sina.farbod@gmail.com>
2024-05-22 13:14:22 +0200

    feat: add WeatherForecast story

---
+++ src/stories/components/WeatherForecast.stories.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import WeatherForecast from "../../app/components/WeatherForecast";
+import { TemperatureProvider } from "../../app/context/TemperatureContext";
+
+export default {
+  title: "Components/WeatherForecast",
+  component: WeatherForecast,
+  decorators: [
+    (Story) => (
+      <TemperatureProvider>
+        <Story />
+      </TemperatureProvider>
+    ),
+  ],
+} as Meta<typeof WeatherForecast>;
+
+type Story = StoryObj<typeof WeatherForecast>;
+
+export const Primary: Story = {
+  args: {
+    forecast: [
+      {
+        date: "2023-05-20",
+        minTemperature: 10,
+        maxTemperature: 20,
+        description: "Cloudy",
+        icon: 6,
+      },
+      {
+        date: "2023-05-21",
+        minTemperature: 12,
+        maxTemperature: 22,
+        description: "Partly Cloudy",
+        icon: 7,
+      },
+    ],
+  },
+};
